1920066de41660c0076f477912ce757712d0a841
AndresVeas <andres.veas@epn.edu.ec>
2025-02-04 16:29:22 -0500

    Pantalla Login corregido

--- src/UserInterface/Form/LoginPanel.java
+++ src/UserInterface/Form/LoginPanel.java
@@ -2,11 +2,10 @@ package UserInterface.Form;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
 
 import UserInterface.CustomerControl.G6Button;
 import UserInterface.CustomerControl.G6Label;
@@ -41,64 +40,72 @@ public class LoginPanel extends JPanel implements ActionListener {
     pnlCredenciales   = new JPanel(new FlowLayout()),
     pnlBtns  = new JPanel(new FlowLayout());    
     private void customizeComponent() {
-
-        backgroundImage = new ImageIcon(getClass().getResource("src\\UserInterface\\Resources\\FondoEstrellas.jpg")).getImage();
+        URL imageUrl = getClass().getResource("/UserInterface/Resources/FondoEstrellas.jpg");
+        backgroundImage = new ImageIcon(imageUrl).getImage();
         
-
-        pnlCredenciales.setBackground(Color.BLACK);
-        pnlBtns.setBackground(Color.BLACK);
-
+        pnlCredenciales.setOpaque(false);
+        pnlBtns.setOpaque(false);
+        
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-
+        
         // Configurar las propiedades de GridBagConstraints
         gbc.insets = new Insets(5, 5, 5, 5); // Margen alrededor de los componentes
         gbc.fill = GridBagConstraints.HORIZONTAL; // Expandir horizontalmente
-
+        
         // Añadir componentes a pnlCredenciales
         pnlCredenciales.setLayout(new GridBagLayout());
         GridBagConstraints gbcCred = new GridBagConstraints();
         gbcCred.insets = new Insets(5, 5, 5, 5);
         gbcCred.fill = GridBagConstraints.HORIZONTAL;
-
+        
         gbcCred.gridx = 0;
         gbcCred.gridy = 0;
         pnlCredenciales.add(lblIniciarSesion, gbcCred);
-
+        
         gbcCred.gridy = 1;
         pnlCredenciales.add(lblUsuario, gbcCred);
-
+        
         gbcCred.gridy = 2;
         pnlCredenciales.add(txtUsuario, gbcCred);
-
+        
         gbcCred.gridy = 3;
         pnlCredenciales.add(lblPassword, gbcCred);
-
+        
         gbcCred.gridy = 4;
         pnlCredenciales.add(txtPassword, gbcCred);
-
+        
         // Añadir componentes a pnlBtns
         pnlBtns.setLayout(new GridBagLayout());
         GridBagConstraints gbcBtns = new GridBagConstraints();
         gbcBtns.insets = new Insets(5, 5, 5, 5);
         gbcBtns.fill = GridBagConstraints.CENTER;
-
+        
         gbcBtns.gridx = 0;
         gbcBtns.gridy = 0;
         pnlBtns.add(btnIngresar, gbcBtns);
-
+        
         gbcBtns.gridy = 1;
         pnlBtns.add(btnRegistro, gbcBtns);
-
+        
         gbcBtns.gridy = 2;
         pnlBtns.add(btnSalir, gbcBtns);
-
+        
         // Añadir pnlCredenciales y pnlBtns al LoginPanel
         gbc.gridx = 0;
         gbc.gridy = 0;
         add(pnlCredenciales, gbc);
-
+        
         gbc.gridy = 1;
         add(pnlBtns, gbc);
     }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
 }
+    
