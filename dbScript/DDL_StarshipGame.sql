9371868fbe0872459f3dd3686ad7a513c5f26757
SabinaZabala <zabalasabina14@gmail.com>
2025-02-03 22:34:57 -0500

    Agrego DDL  y archivos para base de datos

---
+++ dbScript/DDL_StarshipGame.sql
@@ -0,0 +1,36 @@
+-- database: ../dataBase/StarshipGame.db
+
+DROP TABLE IF EXISTS LoginUs;
+DROP TABLE IF EXISTS Score;
+DROP TABLE IF EXISTS Jugador;
+
+
+CREATE TABLE Jugador (
+     idJugador        INTEGER PRIMARY KEY AUTOINCREMENT
+    ,nombre           VARCHAR(15) NOT NULL
+    ,apellido         VARCHAR(15) NOT NULL
+    ,contrasena       VARCHAR(15) NOT NULL
+    ,puntajeMax       INTEGER DEFAULT 0
+    ,estado           VARCHAR(1) DEFAULT 'A'
+    ,FechaCreacion    DATETIME NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+    ,FechaModifica    DATETIME DEFAULT (DATETIME('now', 'localtime'))
+);
+
+
+CREATE TABLE Score (
+     idScore         INTEGER PRIMARY KEY AUTOINCREMENT
+    ,idJugador       INTEGER NOT NULL REFERENCES Jugador (idJugador)
+    ,puntaje         INTEGER DEFAULT 0
+    ,estado          VARCHAR(1) DEFAULT 'A'
+    ,FechaCreacion   DATETIME NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+    ,FechaModifica   DATETIME DEFAULT (DATETIME('now', 'localtime'))
+);
+
+
+CREATE TABLE LoginUs (
+     idLogin         INTEGER PRIMARY KEY AUTOINCREMENT
+    ,idJugador       INTEGER NOT NULL   REFERENCES Jugador (idJugador)
+    ,estado          VARCHAR(1) DEFAULT 'A'
+    ,FechaCreacion   DATETIME NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+    ,FechaModifica   DATETIME DEFAULT (DATETIME('now', 'localtime'))
+);
